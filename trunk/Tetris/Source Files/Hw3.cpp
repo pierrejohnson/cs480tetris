3d6ca1b17281cedf803375dfd59209100eaf16f5
danx255 <danx255@f0761a76-1b4b-0410-a3c3-d1b84f88fa16>
2008-04-24 04:47:22 +0000

    Added pause functionality

--- trunk/Tetris/Source Files/Hw3.cpp
+++ trunk/Tetris/Source Files/Hw3.cpp
@@ -7,33 +7,36 @@
 //#include <GLUT/glut.h>
 //#include <OpenGL/glu.h>
 Tetris game;
+bool pause = false;
 
 //Timer function
 //Calls the game update function
-void update(int value){
+void update(int value) {
+	if(!pause){
 	//game.test();
 	game.update();
 	glutPostRedisplay();
+	}
 	//every 1000 ms (1s) the "update(1)" fn is called
-	glutTimerFunc(1000, update, 1);	
+	glutTimerFunc(1000, update, 1);
 }
 
 /*Initialization function
  * Initialize material property, light source, 
  * lighting model, and depth buffer.
-*/
+ */
 void init(void) {
-	
+
 	game.Initialize();
 	//every 1 seconds calls the update function
 	glutTimerFunc(1000, update, 1);
-	
+
 }
 
 //Keyboard function
 //When key is pressed, perform action here
 void keyboard(unsigned char key, int x, int y) {
-	switch(key){
+	switch (key) {
 	case 'a':
 		game.moveLeft();
 		glutPostRedisplay();
@@ -55,13 +58,18 @@ void keyboard(unsigned char key, int x, int y) {
 		game.getBlock();
 		glutPostRedisplay();
 		break;
+	case 'p':
+		if(pause)
+			pause = false;
+		else
+			pause = true;
 	default:
 		break;
-		
+
 	}
 }
-void spkeyboard(int key, int x, int y){
-	switch(key){
+void spkeyboard(int key, int x, int y) {
+	switch (key) {
 	case GLUT_KEY_LEFT:
 		cout<<"Move Left"<<endl;
 		game.moveLeft();
@@ -81,10 +89,9 @@ void spkeyboard(int key, int x, int y){
 		break;
 	default:
 		break;
-		
+
 	}
 }
-
 
 //Display function
 //When screen is redrawn, redraw here
@@ -93,83 +100,87 @@ void display(void) {
 	glLoadIdentity();
 	glColor3f(1.0f, 1.0f, 1.0f);
 	glBegin(GL_QUADS);
-	glVertex3f(-0.7f,-1.2f,-6.0f);
-	glVertex3f(-0.7f,1.2f,-6.0f);
-	glVertex3f(0.6f,1.2f,-6.0f);
-	glVertex3f(0.6f,-1.2f,-6.0f);
+	glVertex3f(-0.7f, -1.2f, -6.0f);
+	glVertex3f(-0.7f, 1.2f, -6.0f);
+	glVertex3f(0.6f, 1.2f, -6.0f);
+	glVertex3f(0.6f, -1.2f, -6.0f);
 	glEnd();
-	for(int i = 0; i<4;i++){
-		for(int j = 0; j<4;j++){
-			int color = game.nextBlock->getBlockMatrix().getCell(i,j);
+	for (int i = 0; i<4; i++) {
+		for (int j = 0; j<4; j++) {
+			int color = game.nextBlock->getBlockMatrix().getCell(i, j);
 			glColor3f(color*0.4f, color*0.1f, color*0.6f);
 			GLfloat x = j;
 			GLfloat y = 18-i;
-			if(color>0 && color<8){
+			if (color>0 && color<8) {
 				glLoadIdentity();
 				glPushMatrix();
-				glTranslated(x*0.1+0.7,y*0.1-1,-5);
-				glScalef(0.1f,0.1f,0.1f);
+				glTranslated(x*0.1+0.7, y*0.1-1, -5);
+				glScalef(0.1f, 0.1f, 0.1f);
 				glutSolidCube(1.0f);
+				glColor3f(0, 0, 0);
+				glutWireCube(1.0f);
 				glPopMatrix();
 			}
 		}
 	}
-	for(int i = 19;i>=0;i--){
-				for(int j = 9; j>=0;j--){
-					int color = game.getFieldCell(i,j);
-					int color1 = game.getPlayCell(i,j);
-					glColor3f(color*0.4f, color*0.1f, color*0.6f);
-					GLfloat x = j;
-					GLfloat y = 18-i;
-					glColor3f(color1*0.4f, color1*0.1f, color1*0.6f);
-					if(color1>0 && color1<8){
-						glLoadIdentity();
-						glPushMatrix();
-						glTranslated(x*0.1-.5,y*0.1-0.8,-5);
-						glScalef(0.1f,0.1f,0.1f);
-						glutSolidCube(1.0f);
-						glColor3f(0,0,0);
-						glutWireCube(1.0f);
-						glPopMatrix();
-					}
-					glColor3f(color*0.4f, color*0.1f, color*0.6f);
-					if(color>0 && color<8){
-						glLoadIdentity();
-						glPushMatrix();
-						glTranslated(x*0.1-.5,y*0.1-0.8,-5);
-						glScalef(0.1f,0.1f,0.1f);
-						glutSolidCube(1.0f);
-						glColor3f(0,0,0);
-						glutWireCube(1.0f);
-						glPopMatrix();
-					}
+	for (int i = 19; i>=0; i--) {
+		for (int j = 0; j<10; j++) {
+			int color = game.getFieldCell(i, j);
+			int color1 = game.getPlayCell(i, j);
+			glColor3f(color*0.4f, color*0.1f, color*0.6f);
+			GLfloat x = j;
+			GLfloat y = 18-i;
+			glColor3f(color1*0.4f, color1*0.1f, color1*0.6f);
+			if (color1>0 && color1<8) {
+				glLoadIdentity();
+				glPushMatrix();
+				glTranslated(x*0.1-.5, y*0.1-0.8, -5);
+				glScalef(0.1f, 0.1f, 0.1f);
+				glutSolidCube(1.0f);
+				glColor3f(0, 0, 0);
+				glutWireCube(1.0f);
+				glPopMatrix();
 
-				}
 			}
+			glColor3f(color*0.4f, color*0.1f, color*0.6f);
+			if (color>0 && color<8) {
+				glLoadIdentity();
+				glPushMatrix();
+				glTranslated(x*0.1-.5, y*0.1-0.8, -5);
+				glScalef(0.1f, 0.1f, 0.1f);
+				glutSolidCube(1.0f);
+				glColor3f(0, 0, 0);
+				glutWireCube(1.0f);
+				glPopMatrix();
+			}
+
+		}
+	}
 	glFlush();
+	glutSwapBuffers();
 }
 
 //Reshape Function
-void reshape(int x, int y)
-{
-	if (y == 0 || x == 0) return;  //Nothing is visible then, so return
-	
+void reshape(int x, int y) {
+	if (y == 0 || x == 0)
+		return; //Nothing is visible then, so return
+
 	//Set a new projection matrix
-	glMatrixMode(GL_PROJECTION);  
+	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	//Angle of view:40 degrees
 	//Near clipping plane distance: 0.5
 	//Far clipping plane distance: 20.0
-	gluPerspective(25.0,(GLdouble)x/(GLdouble)y,1,10.0);
+	gluPerspective(25.0, (GLdouble)x/(GLdouble)y, 1, 10.0);
 
 	glMatrixMode(GL_MODELVIEW);
-	glViewport(0,0,x,y);  //Use the whole window for rendering
+	glViewport(0, 0, x, y); //Use the whole window for rendering
 }
 
 //Main function
 //Sets up window and callback funcions
 int main(int argc, char **argv) {
-	glutInitDisplayMode(GLUT_SINGLE);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
 	glutInitWindowPosition(50, 50);
 	glutInitWindowSize(700, 700);
 	glutInit(&argc, argv);
